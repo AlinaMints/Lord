b4b7e540a97ea60429035655f1876cabf7cdf05c
Alina <toivonen.alina@yandex.ru>
2021-01-13 12:18:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lord/ForText.java
@@ -0,0 +1,21 @@
+package com.example.lord;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ForText extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_for_text);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("message");
+        TextView messageText = (TextView) findViewById(R.id.messageText);
+        messageText.setText(message);
+    }
+}
